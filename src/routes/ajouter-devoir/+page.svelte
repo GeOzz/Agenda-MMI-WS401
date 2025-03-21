8e1e255064ac3f8e9524b71eb17f40b5fcd6a29d
N3X-AGENCY <n3x503@gmail.com>
2025-03-21 19:53:19 +0700

    Ajout du tri des matières filtrées par ID pour chaque promotion dans le composant ajouter-devoir

--- src/routes/ajouter-devoir/+page.svelte
+++ src/routes/ajouter-devoir/+page.svelte
@@ -104,22 +104,30 @@ Ceci est un paragraphe avec du texte **en gras** et *en italique*.
 
 	let MATIERES_FILTRE = $derived.by(() => {
 		if (promotion === '1ère Année (BUT1)') {
-			return matieres_options.filter((matiere) => {
-				const ID = parseInt(String(matiere.id).slice(2, 5));
-				return ID >= 100 && ID <= 300;
-			});
+			return matieres_options
+				.filter((matiere) => {
+					const ID = parseInt(String(matiere.id).slice(2, 5));
+					return ID >= 100 && ID <= 300;
+				})
+				.sort((a, b) => parseInt(String(a.id).slice(2, 5)) - parseInt(String(b.id).slice(2, 5)));
 		} else if (promotion === '2ème Année (BUT2)') {
-			return matieres_options.filter((matiere) => {
-				const ID = parseInt(String(matiere.id).slice(2, 5));
-				return ID >= 300 && ID <= 500;
-			});
+			return matieres_options
+				.filter((matiere) => {
+					const ID = parseInt(String(matiere.id).slice(2, 5));
+					return ID >= 300 && ID <= 500;
+				})
+				.sort((a, b) => parseInt(String(a.id).slice(2, 5)) - parseInt(String(b.id).slice(2, 5)));
 		} else if (promotion === '3ème Année (BUT3)') {
-			return matieres_options.filter((matiere) => {
-				const ID = parseInt(String(matiere.id).slice(2, 5));
-				return ID >= 500 && ID <= 700;
-			});
+			return matieres_options
+				.filter((matiere) => {
+					const ID = parseInt(String(matiere.id).slice(2, 5));
+					return ID >= 500 && ID <= 700;
+				})
+				.sort((a, b) => parseInt(String(a.id).slice(2, 5)) - parseInt(String(b.id).slice(2, 5)));
 		}
-		return matieres_options.filter((matiere) => matiere.id !== '1');
+		return matieres_options
+			.filter((matiere) => matiere.id !== '1')
+			.sort((a, b) => parseInt(String(a.id).slice(2, 5)) - parseInt(String(b.id).slice(2, 5)));
 	});
 </script>
 
